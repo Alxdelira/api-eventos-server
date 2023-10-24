390bc782a37aef7f92af3ac704b8e3e905ae0ecb
alxdelira <alx.delira@gmail.com>
2023-10-23 23:29:20 -0400

    server ajusting

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const jsonServer = require('json-server')
 
 const server = jsonServer.create()
+
 // Uncomment to allow write operations
 // const fs = require('fs')
 // const path = require('path')
@@ -9,18 +10,20 @@ const server = jsonServer.create()
 // const db = JSON.parse(data);
 // const router = jsonServer.router(db)
 
+// Comment out to allow write operations
 const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
+// Add this before server.use(router)
 server.use(jsonServer.rewriter({
-    '/eventos/*': '/$1',
-    
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
 }))
 server.use(router)
-server.listen(3001, () => {
-    console.log('JSON Server está Rodando!')
+server.listen(3000, () => {
+    console.log('JSON Server is running')
 })
 
 // Export the Server API
